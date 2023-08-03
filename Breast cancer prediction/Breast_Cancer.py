67b95467a4f7c63eb13b008caed4b41e78f9f60e
AayushJha09 <89629707+AayushJha09@users.noreply.github.com>
2023-08-03 14:59:22 +0530

    Add files via upload

---
+++ Breast cancer prediction/Breast_Cancer.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("D:/Yatin/Breast-Cancer-Detection-Web-App-main/Breast-Cancer-Detection-Web-App-main/Breast cancer prediction/data.csv")
+print(df.head())
---
+++ Breast cancer prediction/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+# import matplotlib.pyplot as plt
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import KFold
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.svm import SVC
+# import seaborn as sns
